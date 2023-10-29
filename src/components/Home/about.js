4c5186fe2c723a0db649d1484b0eb319810deb76
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-10-30 02:50:22 +0530

    Added New features

---
+++ src/components/Home/about.js
@@ -0,0 +1,22 @@
+const about = [
+    {
+        "id": 1,
+        "heading": "Cost-Efficient Trading",
+        "description": "LQX identifies the optimal liquidity path, reducing overall expenses for traders and ensuring seamless position initiation.",
+        "image": "1.png"
+    },
+    {
+        "id": 2,
+        "heading": "Intelligent Position Allocation",
+        "description": "LQX intelligently directs positions to appropriate underlying trading protocols, considering market conditions, position dimensions, and user choices",
+        "image": "2.png"
+    },
+    {
+        "id": 3,
+        "heading": "Enhanced Leverage",
+        "description": "While LQX offers up to 100x leverage within its native pool, it can also provide extra margin for traders to achieve up to 100x leverage on integrated underlying protocols",
+        "image": "3.png"
+    }
+]
+
+export default about;
